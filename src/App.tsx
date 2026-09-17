eb04e2aaf5663a87d693ed9dcf84e522d7549538
YuliaSabitova <yuliasab2017@mail.com>
2026-09-17 10:11:30 +0300

    Changes added

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,40 @@ import './App.scss';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 
-
 export const App = () => {
   const location = useLocation();
+
   return (
     <>
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/" className={classNames({ 'is-active': location.pathname === '/' })}>
-            Home
-          </Link>
-          <Link to="/tabs" className={classNames({ 'is-active': location.pathname.startsWith('/tabs') })}>
-            Tabs
-          </Link>
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/"
+              className={classNames('navbar-item', { 'is-active': location.pathname === '/' })}
+            >
+              Home
+            </Link>
+            <Link
+              to="/tabs"
+              className={classNames('navbar-item',{
+                'is-active': location.pathname.startsWith('/tabs'),
+              })}
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Outlet />
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Outlet />
-      </div>
-    </div>
-  </>
-);
-}
+    </>
+  );
+};
